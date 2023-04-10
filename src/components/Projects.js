a4354bbdf8fd020678eddac18e65284b742728af
Josiah ENUMAH <josiahenumah94@gmail.com>
2023-04-10 23:10:41 +0100

    added projects and updated resume file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,37 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import olympians from "../assets/olympians.png";
+import theolympiansmall from "../assets/theolympiansmall.png";
+import kingCabana from "../assets/kingCabana.png";
 
 const Projects = () => {
+  const projects = [
+    {
+      title: "King Cabana",
+      description:
+        "An event management web-app, which facilitates efficient connections between event organizers, attendees, and corporate sponsors, with the goal of increasing their chances of meeting their objectives.",
+      site: "https://kingcabana.com",
+      repo: "https://github.com/King-cabana",
+      img: kingCabana,
+    },
+    {
+      title: "The Olympians Mall - ecommerce app",
+      description:
+        "Allows users to shop online, browse product catalogs, add items to a cart, and order management capabilities.",
+      site: "https://theolympiansmall.netlify.app",
+      repo: "https://github.com/JosiahENUMAH/The-Olympians-Mall-eCommerce-store",
+      img: theolympiansmall,
+    },
+    {
+      title: "OLYMPIANS - Global Community",
+      description:
+        "Website for Olympians Community with exquisite features and responsive design.",
+      site: "https://olympians.netlify.app",
+      repo: "https://github.com/JosiahENUMAH/olympians-web",
+      img: olympians,
+    },
+  ];
+
   return (
     <>
       <div name="Projects" className="w-full h-full">
@@ -17,50 +46,56 @@ const Projects = () => {
               />
             </p>
           </div>
-
-          <div className="flex flex-col items-center justify-center mt-10 md:flex-row gap-10">
-            <div
-              className="w-2/3 md:w-1/2 p-4 md:p-8 bg-blue-900 md:mr-10 rounded-md hover:rounded-3xl duration-300"
-              data-aos="fade-right"
-              data-aos-duration="1000"
-            >
-              <img src={olympians} alt="olympians" className="full" />
-            </div>
-
-            <div
-              className="flex flex-col justify-between w-2/3 md:w-1/2  py-2"
-              // data-aos="fade-left"
-              // data-aos-duration="1000"
-            >
-              <p className="font-bold text-lg md:text-2xl">
-                OLYMPIANS - Global Community
-              </p>
-              <p className="md:w-[20rem]">
-                Website for Olympians Community with exquisite features and
-                responsive design.
-              </p>
-              <br />
-              <div className="flex justify-center md:justify-start">
-                <a
-                  href="https://olympians.netlify.app"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="mr-4 md:mr-10 text-blue-800 font-bold border-2 px-2 py-1 rounded-md hover:bg-gray-200 duration-300 text-sm md:text-lg"
+          {projects.map((project, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center mt-10 md:flex-row gap-10"
+              >
+                <div
+                  className="w-2/3 md:w-1/2 p-4 md:p-8 bg-blue-900 md:mr-10 rounded-md hover:rounded-3xl duration-300"
+                  data-aos="fade-right"
+                  data-aos-duration="1000"
                 >
-                  Live site
-                </a>{" "}
-                <a
-                  href="https://github.com/JosiahENUMAH/olympians-web"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-blue-800 font-bold border-2 px-2 py-1 rounded-md hover:bg-gray-200 duration-300 text-sm md:text-lg"
+                  <img
+                    src={project.img}
+                    alt="project display"
+                    className="full"
+                  />
+                </div>
+                <div
+                  className="flex flex-col justify-between w-2/3 md:w-1/2  py-2"
+                  // data-aos="fade-left"
+                  // data-aos-duration="1000"
                 >
-                  Code base
-                </a>
+                  <p className="font-bold text-lg md:text-2xl">
+                    {project.title}
+                  </p>
+                  <p className="md:w-[20rem]">{project.description}</p>
+                  <br />
+                  <div className="flex justify-center md:justify-start">
+                    <a
+                      href={project.site}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="mr-4 md:mr-10 text-blue-800 font-bold border-2 px-2 py-1 rounded-md hover:bg-gray-200 duration-300 text-sm md:text-lg"
+                    >
+                      Live site
+                    </a>
+                    <a
+                      href={project.repo}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-blue-800 font-bold border-2 px-2 py-1 rounded-md hover:bg-gray-200 duration-300 text-sm md:text-lg"
+                    >
+                      Code base
+                    </a>
+                  </div>
+                  <br />
+                </div>
               </div>
-              <br />
-            </div>
-          </div>
+            );
+          })}
 
           <footer
             className="flex justify-center mt-0 md:mt-4"
